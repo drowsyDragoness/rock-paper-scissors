8159d201ecc01680f5c525710a532df537456f51
drowsyDragoness <drowsydragoness@gmail.com>
2022-12-20 21:35:10 -0500

    Started implementing gameplay loop function

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let i = Math.floor((Math.random() * 3));
+    if (i === 0) {
+        return "Rock!";
+    } else if (i === 1) {
+        return "Paper!";
+    } else {
+        return "Scissors!";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection ===)
+}
+
+const playerSelection = 'Rock'
+
+console.log(getComputerChoice())
