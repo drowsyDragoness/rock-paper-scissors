28bb83f403868dff9a5f5ca548d65f6201e1930d
drowsyDragoness <drowsydragoness@gmail.com>
2022-12-20 23:03:19 -0500

    Fixed playRound and getRandomChoice functions.

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-function getComputerChoice() {
+function getRandomChoice() {
     let i = Math.floor((Math.random() * 3));
     if (i === 0) {
-        return "Rock!";
+        return "Rock";
     } else if (i === 1) {
-        return "Paper!";
+        return "Paper";
     } else {
-        return "Scissors!";
+        return "Scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection ===)
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } else if (playerSelection === 'Rock') {
+        if (computerSelection === 'Paper') {
+            return 'Paper beats Rock. You lose!';
+        } else {
+            return 'Rock beats Scissors. You win!';
+        }
+    } else if (playerSelection === 'Paper') {
+        if (computerSelection === 'Rock') {
+            return 'Paper beats Rock. You win!';
+        } else {
+            return 'Scissors beats Paper. You lose!';
+        }
+    } else if (playerSelection === 'Scissors') {
+        if (computerSelection === 'Rock') {
+            return 'Rock beats Scissors. You lose!';
+        } else {
+            return 'Scissors beats Paper. You win!';
+        }
+    }
 }
 
-const playerSelection = 'Rock'
 
-console.log(getComputerChoice())
+const playerSelection = getRandomChoice();
+const computerSelection = getRandomChoice();
+
+console.log(`You chose: ${playerSelection}`);
+console.log(`Computer chose: ${computerSelection}`);
+console.log(playRound(playerSelection, computerSelection))
